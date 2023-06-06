4a9b95667cffbad70f53b58ff9bcb1e443422577
Shehab Ihab <shehabihab@hotmail.com>
2023-06-06 16:49:51 +0300

    0x01-fixed 3,6,8,9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
-        print("{}".format(i), end="")
+        print("{}".format(i))
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         last_digit = (number * -1) % 10
-        last_digit = last_digit * -1
+        last_digit = last_digit * 1
     else:
         last_digit = number % 10
     print("{}".format(last_digit), end="")
